693990db79a0337533f19a2c5e0e929090d85daa
nbarabash <nbarabash@sharethis.com>
2014-02-18 19:03:07 +0200

    1. fixed typo 2. added assembly plugin and assembly.xml to build tar.gz archive with jar file

--- src/main/java/com/sharethis/mongodb/exception/MigrationScriptNotFoundException.java
+++ src/main/java/com/sharethis/mongodb/exception/MigrationScriptNotFoundException.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 import java.text.MessageFormat;
 
 public class MigrationScriptNotFoundException extends FileNotFoundException {
-    private static final String MESSAGE = "Mongo migration script {0} wa not found.";
+    private static final String MESSAGE = "Mongo migration script {0} was not found.";
 
     public MigrationScriptNotFoundException(String scriptPath) {
         super(MessageFormat.format(MESSAGE, scriptPath));
